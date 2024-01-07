e856fe3a3ae4b1189996f0cb7dfdf833baefd6d7
Lucas Araújo De oliveira <101775867+lucas0302@users.noreply.github.com>
2024-01-07 12:54:05 -0300

    limpando os comentários

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import Pokemon from "./components/Pokemon";
 export default function App({ pokemons }) {
   const [selecionados, setSelecionados] = useState([]);
 
-  // Cada pokémon possui as propriedades nome e número
-  // Ex: { nome: "bulbasaur", numero: 1 }
-
   function clicouCard(pokemon) {
     const estaSelecionado = selecionados.includes(pokemon);
     
@@ -21,7 +18,6 @@ export default function App({ pokemons }) {
 
       <ul className="lista">
         {pokemons.map((p) => (
-          // adicionar clique e classe CSS no li (nome da classe: selecionado)
           <li key={p.nome} onClick={() => clicouCard(p.nome)} className={`pokecard ${selecionados.includes(p.nome) ? "selecionado" : ""}`}>
             <Pokemon pokemon={p} />
           </li>
